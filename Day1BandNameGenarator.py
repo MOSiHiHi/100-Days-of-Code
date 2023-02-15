0cd6704a8b46beee29d8fa430414cd46eb73bbd7
MOSiHiHi <MostafaJobrani@Gmail.Com>
2023-02-15 20:59:59 +0330

    Initial commit.

---
+++ Day1BandNameGenarator.py
@@ -0,0 +1,14 @@
+# The code I wrote for day 1 challeng:
+
+print("Hi bro, I'm gonna generate you band names.")
+city = input("What's you city name?!\n")
+pet = input("What's you pet name?!\n")
+band_name = city + " " + pet
+print("Your band name is " + band_name)
+
+# The code writen by the author:
+
+print("Welcome to the band name generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet.\n")
+print("Your band name could be " + city + " " + pet)
