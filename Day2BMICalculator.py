1e8621244459124e1c92b2c630ef932453cdc9de
MOSiHiHi <MostafaJobrani@Gmail.Com>
2023-02-17 04:26:13 +0330

    First commit.

---
+++ Day2BMICalculator.py
@@ -0,0 +1,25 @@
+# The code written by me.
+
+weight = input("Type your weight in kg: ")
+height = input("Type your height in m: ")
+weight = float(weight)
+height = float(height)
+bmi = weight/height**2
+bmi = int(bmi)
+print(bmi)
+
+# The code written by author.
+
+weight = input("Type your weight in kg: ")
+height = input("Type your height in m: ")
+bmi = int(weight)/float(height)**2
+bmi_as_int = int(bmi)
+print(bmi_as_int)
+
+# My code after I watched the author's code.
+
+weight = input("Type your weight in kg: ")
+height = input("Type your height in m: ")
+bmi = int(weight)/float(height)**2
+bmi = int(bmi)
+print(bmi)
