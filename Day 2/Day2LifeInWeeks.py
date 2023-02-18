386b0655f679460d54a936ec7b9f9c92df1d128a
MOSiHiHi <MostafaJobrani@Gmail.Com>
2023-02-19 03:13:55 +0330

    Added some author' codes

--- Day 2/Day2LifeInWeeks.py
+++ Day 2/Day2LifeInWeeks.py
@@ -1,6 +1,22 @@
+# My code
+
 age = input("What is your current age? ")
 yearsLeft = 90 - int(age)
 day = yearsLeft * 365
 week = yearsLeft * 52
 month = yearsLeft * 12
 print(f"You have {yearsLeft} years or {day} days, {week} weeks, and {month} months left.")
+
+# Author's code
+
+age = input("What is your current age? ")
+age_as_int = int(age)
+years_remaining = 90 - age_as_int
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining * 12
+massage = f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left."
+print(massage)
+
+# My code
+# I see no way to improve...
--- Day 2/Day2TipCalculator.py
+++ Day 2/Day2TipCalculator.py
@@ -1,6 +1,33 @@
+# # My code
+
 print("Welcome to the tip calculator.")
 totalBill = int(input("how much was the total bill? "))
-tip = round(int(input("What percentage tip would you like to give? "))/100, 2)
+tip = round(int(input("What percentage tip would you like to give? "))/100)
 people = int(input("How many people to split the bill? "))
 payforperson = round((totalBill/people) * (1.00 + tip), 2)
 print(payforperson)
+
+# The author' code
+
+print("Welcome to the tip calculator.")
+bill = float(input("how much was the total bill? "))
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
+final_amount = "{:.2f}".format(bill_per_person)
+print(f"Each person should pay {bill_per_person}$")
+
+# My code improved
+
+print("Welcome to the tip calculator.")
+bill = int(input("how much was the total bill? "))
+tip = round(int(input("What percentage tip would you like to give? "))/100)
+people = int(input("How many people to split the bill? "))
+payforperson = round((bill/people) * (1.00 + tip), 2)
+payforperson = "{:.2f}".format(payforperson)
+print(payforperson)
+
